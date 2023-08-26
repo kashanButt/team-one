52e51794524a974ef0eaa460dd07ae94764f1bae
kashanButt <kashanbutt566@gmail.com>
2023-08-26 23:51:35 +0500

    updated config

--- config/settings.php
+++ config/settings.php
@@ -1,17 +1,17 @@
 <?php
 $settings = [
-    'site' => [
-        'base_url' => 'http://localhost/projects/team-one/',
-        'file_url' => '/projects/team-one/',
-        'site_name' => 'Team One',
-        'seperator' => '-',
-    ]
     // 'site' => [
-    //     'base_url' => 'https://teamonemanagementgrp.com/',
-    //     'file_url' => '/',
+    //     'base_url' => 'http://localhost/projects/team-one/',
+    //     'file_url' => '/projects/team-one/',
     //     'site_name' => 'Team One',
     //     'seperator' => '-',
     // ]
+    'site' => [
+        'base_url' => 'https://teamonemanagementgrp.com/',
+        'file_url' => '/',
+        'site_name' => 'Team One',
+        'seperator' => '-',
+    ]
 ];
 
 return $settings;
